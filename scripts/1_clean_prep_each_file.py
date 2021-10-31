8ca2d0b0815cfd10e5e3c767a64f8d43d0683e57
Miller,Sean <Sean.Miller@Cerner.com>
2021-10-30 21:12:22 -0500

    make better .py scripts

--- scripts/1_clean_prep_each_file.py
+++ scripts/1_clean_prep_each_file.py
@@ -1,35 +1,37 @@
 from glob import glob
 import os
 import pandas as pd
+from datetime import timedelta
 
 
-directory = 'input directory'
-outpath = 'output path'
+directory = '/Users/sm029588/Google Drive/Spotify/Weekly'
+outpath = '/Users/sm029588/Google Drive/Spotify/Weekly_Clean'
 os.chdir(directory)
 header_list = ['chart_position', 'song', 'performer', 'streams', 'spotify_url']
-for filename in glob('filename or wildcard match'):
+for filename in glob('*.csv'):
     df = pd.read_csv(filename, names=header_list)
 
-    #get rid of junk rows
+    # get rid of junk rows
     df = df.iloc[2:]
 
-    #add a column with a csv name
+    # add a column with a csv name
     df["origin"] = filename
 
-    #Create Song identifier
+    # Create Song identifier
     df["song_id"] = df['song']+df['performer']
 
-    #grab the region
+    # grab the region
     df['region'] = df['origin'].str.split('-').str[1]
     region = df['region']
 
-    #Extract Spotify URI from the URL
+    # Extract Spotify URI from the URL
     df['spotify_uri'] = df['spotify_url'].str[-22:]
 
-    #Extract Date from file path name
-    df['chart_date'] = pd.to_datetime(df['origin'].str.extract('(\d{4}-\d{2}-\d{2})')[0])
+    # Extract Date from file path name
+    # df['chart_date'] = pd.to_datetime(df['origin'].str.extract('(\d{4}-\d{2}-\d{2})')[0])
+    df['chart_dates'] = pd.to_datetime(df['origin'].str.extract('(\d{4}-\d{2}-\d{2})')[0])
+    df['chart_date'] = df['chart_dates'] + timedelta(days=6)
 
-
-    df.to_csv(outpath+filename, index=False)
+    df.to_csv(outpath+'/'+filename, index=False, columns=['chart_position', 'song', 'performer', 'streams', 'spotify_url', 'origin', 'song_id', 'region', 'spotify_uri', 'chart_date'])
 
     print(filename)
--- scripts/2_append_files_per_region.py
+++ scripts/2_append_files_per_region.py
@@ -1,18 +1,24 @@
+from glob import glob
+import os
 import pandas as pd
-import glob
+
 
 # Open an empty dataframe
 df_all = pd.DataFrame()
 
 # Find files that match wild card by region
-for f in glob.glob("input path/filename or wildcard match"):
+directory = '/Users/sm029588/Google Drive/Spotify/Daily_Clean'
+outpath = '/Users/sm029588/Google Drive/Spotify/Daily_Ready'
+region = 'at'
+time = 'daily'
+os.chdir(directory)
+for f in glob('*'+region+'-'+time+'*.csv'):
 
     # make sure to apply correct settings (sep, parse_dates, headers, missing_values)
     df = pd.read_csv(f, parse_dates=['chart_date'])
 
     # append each file to the "master" dataframe
     df_all = df_all.append(df)
-    region = df_all['region']
 
     # once all files are appended, we need to sort and reset the index
     df_all.sort_values(['song_id', 'chart_date'], ascending=[True, True], inplace=True)
@@ -22,6 +28,6 @@ for f in glob.glob("input path/filename or wildcard match"):
     df_all['chart_debut'] = df_all.groupby('song_id')["chart_date"].transform('min')
 
     # Time to output but only certain fields
-    df_all.to_csv('output merged filename', index=False, columns=['chart_position', 'chart_date', 'song', 'performer', 'streams', 'spotify_uri', 'song_id', 'region', 'chart_debut'])
+    df_all.to_csv(outpath+'/'+time+'_'+region+'_merged.csv', index=False, columns=['chart_position', 'chart_date', 'song', 'performer', 'streams', 'spotify_uri', 'song_id', 'region', 'chart_debut'])
 
     print(f)
